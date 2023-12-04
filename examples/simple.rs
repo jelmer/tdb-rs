c55eacd412bbe75696e6bf2c06a19bdcfa8217d2
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-12-04 13:53:49 +0000

    Add more examples

---
+++ examples/simple.rs
@@ -0,0 +1,8 @@
+use trivialdb::{Tdb,Flags};
+
+fn main() {
+    let mut tdb = Tdb::open("simple.db", None, Flags::empty(), trivialdb::O_CREAT|trivialdb::O_RDWR, 0o644).unwrap();
+
+    tdb.store(b"key", b"value", None).unwrap();
+    assert_eq!(Some(b"value".to_vec()), tdb.fetch(b"key").unwrap());
+}
